d05b3da26b4784527418e5ebca7aa1b4f42edfdb
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-07-23 16:07:28 +0200

    Extend prettier and eslint config with astro

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+// import { FlatCompat } from "@eslint/eslintrc"
 // Globals for browser code
 import globals from 'globals'
 
@@ -9,19 +10,13 @@ import promisePlugin from 'eslint-plugin-promise'
 import standardJsConfig from 'eslint-config-standard'
 import prettierConfig from 'eslint-config-prettier'
 
+// Astro
+import astroPlugin from 'eslint-plugin-astro'
+import astroParser from 'astro-eslint-parser'
+
 export default [
   {
     ignores: ['.astro/**/*', 'dist/**/*'],
-  },
-
-  // Browser code in src
-  {
-    files: ['src/**/*'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
   },
 
   // Standard style
@@ -37,10 +32,38 @@ export default [
     },
   },
 
+  // Astro components
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: astroParser,
+    },
+    plugins: {
+      astro: astroPlugin,
+    },
+    processor: 'astro/astro',
+    rules: {
+      ...astroPlugin.configs.recommended.rules,
+    },
+  },
+
+  // Client-side scripts in Astro components
+  {
+    files: ['**/*.astro/*.js', '*.astro/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
   // Disable rules interferring with prettier
   {
     rules: {
       ...prettierConfig.rules,
-    }
+    },
   },
 ]
--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,8 @@
+import * as astroPlugin from 'prettier-plugin-astro'
+
 /** @type {import('prettier').Options} */
 export default {
+  plugins: [astroPlugin],
   singleQuote: true,
   semi: false,
 }
