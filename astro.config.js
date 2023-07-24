32cf9887f52afb0f88e7ac7c2a0a8c0d1cb1be67
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-07-24 16:35:10 +0200

    Add svelte integration to astro project

--- astro.config.js
+++ astro.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import { remarkHeadingId } from 'remark-custom-heading-id'
 import sitemap from '@astrojs/sitemap'
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://infolektuell.de',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte()],
   markdown: {
     remarkPlugins: [remarkHeadingId],
   },
