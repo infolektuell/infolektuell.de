b0260458715210f712c3cb258d1b660717258e85
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-07-27 13:59:54 +0200

    Update non-breaking dev deps

--- src/lib/schemas/article.ts
+++ src/lib/schemas/article.ts
@@ -1,11 +1,34 @@
-import { z } from 'zod'
-import { pageData } from './page'
+// noinspection SpellCheckingInspection
 
-export const articleData = pageData.extend({
+import { z } from 'zod'
+import site from '@config/site'
+import { type LanguageDefinition, languages } from '@config/languages'
+
+export const pageData = z.object({
+  title: z.string(),
   subtitle: z.string().optional(),
+  description: z.string().default(site.description),
   lastModified: z.coerce.date(),
   published: z.coerce.date(),
+  lang: z
+    .string()
+    .default(site.defaultLanguage)
+    .transform((val, ctx): LanguageDefinition => {
+      const languageDefinition = languages[val]
+      if (!languageDefinition) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: `${val} is no defined language`,
+        })
+        return z.NEVER
+      }
+      return languageDefinition
+    }),
+  robots: z
+    .tuple([z.enum(['index', 'noindex']), z.enum(['follow', 'nofollow']), z.enum(['archive', 'noarchive'])])
+    .default(['index', 'follow', 'archive']),
+  image: z.string().optional(),
   minutesRead: z.number().min(1),
 })
 
-export type ArticleData = z.infer<typeof articleData>
+export type PageData = z.infer<typeof pageData>
