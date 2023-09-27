7e19db98818342347c1858418432ddfaceb1c5cf
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-09-27 18:44:48 +0200

    Add authors collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,19 @@
 import { defineCollection } from 'astro:content'
+import { authorSchema } from '@lib/schemas/author'
 import { trainingData } from '@lib/schemas/training'
+
+const authors = defineCollection({
+  type: 'data',
+  schema: ({ image }) => {
+    return authorSchema.extend({
+      photo: image().optional(),
+    })
+  },
+})
 
 const trainings = defineCollection({
   type: 'content',
   schema: trainingData,
 })
 
-export const collections = { trainings }
+export const collections = { authors, trainings }
---
+++ src/lib/schemas/author.ts
@@ -0,0 +1,34 @@
+import { z } from 'astro:content'
+
+const nameSchema = z.object({
+  prefix: z.string().optional(),
+  family: z.string(),
+  given: z.string(),
+  additional: z.string().optional(),
+  suffix: z.string().optional(),
+})
+export type Name = z.infer<typeof nameSchema>
+
+const addressSchema = z.object({
+  office: z.string().optional(),
+  extended: z.string().optional(),
+  street: z.string(),
+  locality: z.string(),
+  region: z.string().optional(),
+  postalCode: z.string(),
+  country: z.string(),
+})
+export type Address = z.infer<typeof addressSchema>
+
+export const authorSchema = z.object({
+  name: nameSchema,
+  title: z.string().optional(),
+  email: z.string().email().optional(),
+  phone: z
+    .string()
+    .regex(/^\+[\d\s]+/g)
+    .optional(),
+  address: addressSchema.optional(),
+})
+
+export type Author = z.infer<typeof authorSchema>
