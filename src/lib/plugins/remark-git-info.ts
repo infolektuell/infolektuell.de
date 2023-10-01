7c72bc1beefe2627798761f1e56e91a274d350b6
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-10-01 16:31:02 +0200

    Remark frontmatter: Add last modified date from file if not in git

--- src/lib/plugins/remark-git-info.ts
+++ src/lib/plugins/remark-git-info.ts
@@ -5,6 +5,7 @@ import type { Node } from 'unist'
 import type { VFile } from 'vfile'
 import type { SimpleGit, SimpleGitOptions, LogResult } from 'simple-git'
 import simpleGit from 'simple-git'
+import { stat } from 'node:fs/promises'
 
 const options: Partial<SimpleGitOptions> = {
   baseDir: process.cwd(),
@@ -18,7 +19,13 @@ const plugin: RemarkPlugin = function remarkGitInfo() {
       return
     }
     const gitLog: LogResult = await git.log({ file: file.path, strictDate: true })
-    data.frontmatter.lastModified = gitLog.latest?.date ?? null
+    data.frontmatter.revisionsTotal = gitLog.total
+    if (gitLog.latest) {
+      data.frontmatter.lastModified = gitLog.latest.date
+    } else {
+      const info = await stat(file.history[0])
+      data.frontmatter.lastModified = info.mtime
+    }
   }
 }
 
