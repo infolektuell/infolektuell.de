1625aab75eab42b050ec3eb3e84e0a5ae9f21a4d
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-11-19 14:15:25 +0100

    Add entities for quiz collection

---
+++ src/entities/quiz.ts
@@ -0,0 +1,78 @@
+import { reference, z } from 'astro:content'
+import { micromark } from 'micromark'
+import { math, mathHtml } from 'micromark-extension-math'
+
+const compileMarkdown = (value: string): string => {
+  if (!value) {
+    return ''
+  }
+  return micromark(value, { extensions: [math()], htmlExtensions: [mathHtml()] })
+}
+
+const markdownSchema = z.string().transform(compileMarkdown)
+
+export const baseQuestionSchema = z.object({
+  text: markdownSchema,
+})
+
+export const levelSchema = z.enum(['right', 'neutral', 'wrong']).default('wrong')
+export type Level = z.infer<typeof levelSchema>
+
+export const choiceSchema = z.object({
+  text: z.string(),
+  level: levelSchema,
+  category: z.string().optional(),
+  feedback: markdownSchema,
+})
+export type Choice = z.infer<typeof choiceSchema>
+
+export const questionFormatSchema = z.enum(['select', 'input'])
+export type QuestionFormat = z.infer<typeof questionFormatSchema>
+
+export const selectQuestionSchema = baseQuestionSchema.extend({
+  format: z.literal(questionFormatSchema.enum.select),
+  amount: z.number().int().positive().default(1),
+  choices: choiceSchema.array(),
+})
+export type SelectQuestion = z.infer<typeof selectQuestionSchema>
+
+const numberInputSchema = z.object({
+  type: z.literal('number'),
+  min: z.number().optional(),
+  max: z.number().optional(),
+  step: z.number().optional(),
+  value: z.number(),
+})
+export type NumberInput = z.infer<typeof numberInputSchema>
+
+const textInputSchema = z.object({
+  type: z.literal('text'),
+  multiline: z.boolean().default(false),
+  minlength: z.number().int().positive().optional(),
+  maxlength: z.number().int().positive().optional(),
+  value: z.string(),
+})
+export type TextInput = z.infer<typeof textInputSchema>
+
+const inputSchema = z.discriminatedUnion('type', [numberInputSchema, textInputSchema])
+
+export const inputQuestionSchema = baseQuestionSchema.extend({
+  format: z.literal(questionFormatSchema.enum.input),
+  trials: z.number().int().positive().default(3),
+  feedback: markdownSchema,
+  input: inputSchema,
+})
+export type InputQuestion = z.infer<typeof inputQuestionSchema>
+
+export const questionSchema = z.discriminatedUnion('format', [selectQuestionSchema, inputQuestionSchema])
+export type Question = z.infer<typeof questionSchema>
+
+export const quizSchema = z.object({
+  title: z.string(),
+  description: z.string().optional(),
+  lang: reference('locales').default('de-DE'),
+  intro: markdownSchema,
+  questions: questionSchema.array().default([]),
+  outro: markdownSchema,
+})
+export type Quiz = z.infer<typeof quizSchema>
