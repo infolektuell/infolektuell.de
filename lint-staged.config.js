732f9e347315b9ef9e0f5d825143da4751a871a2
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-11-20 12:27:29 +0100

    Update layouts to use hero component

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@
 
 export default {
   '*.{json,yaml,yml}': ['prettier -w -u'],
-  '*.{js,mjs,cjs,ts,astro,svelte}': ['prettier -w -u', 'eslint --fix'],
+  '*.{js,mjs,cjs,ts}': ['prettier -w -u', 'eslint --fix'],
   '*.{md,mdx}': ['prettier -w -u', 'markdownlint --fix'],
 }
