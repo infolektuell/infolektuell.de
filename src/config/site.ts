d5786485f65cda1765fd7cf61c32c065ec54f000
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2023-12-12 13:28:56 +0100

    Shorten site headline

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ export default {
   defaultLanguage: 'de-DE',
   title: 'Infolektuell',
   description: 'Training und Beratung für Informationskompetenz',
-  headline: 'Mehr Digitale Kompetenz und Selbstbestimmung',
+  headline: 'Digital kompetent und selbstbestimmt',
   summary:
     'Training, Beratung und Dienstleistungen rund um das Leben und Arbeiten mit digitalen Technologien sowie Data Science und Data Literacy',
 }
