23842d2dabbe9effd5f25892c547c152058f4fda
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-01-15 12:32:02 +0100

    Add RSS feed

---
+++ src/pages/blog/rss.xml.ts
@@ -0,0 +1,50 @@
+import type { APIRoute } from 'astro'
+import { getImage } from 'astro:assets'
+import { getCollection } from 'astro:content'
+import rss, { type RSSFeedItem } from '@astrojs/rss'
+import { micromark } from 'micromark'
+import sanitizeHtml from 'sanitize-html'
+import siteConfig from '@config/site'
+import { dedent } from 'ts-dedent'
+import Logo from '@assets/images/logo.png'
+
+export const GET: APIRoute = async function ({ site, generator, url }) {
+  const link = site ?? 'https://infolektuell.de'
+  const image = await getImage({ src: Logo })
+  const imageUrl = url.origin + image.src
+  const posts = await getCollection('posts')
+  posts.sort((a, b) => b.data.publishedTime.valueOf() - a.data.publishedTime.valueOf())
+  const items: RSSFeedItem[] = await Promise.all(
+    posts.map(({ slug, data, body }) => {
+      const content = micromark(body)
+      const { title, headline: description } = data
+      return {
+        title,
+        description,
+        pubDate: data.publishedTime,
+        link: `/blog/${slug}`,
+        content: sanitizeHtml(content),
+      }
+    }),
+  )
+  return rss({
+    title: siteConfig.title,
+    description: siteConfig.description,
+    site: link,
+    items,
+
+    customData: dedent`
+      <language>de-de</language>
+      <generator>${generator}</generator>
+      <docs>https://www.rssboard.org/rss-specification</docs>
+      <managingEditor>info@infolektuell.de (Tamara Cook)</managingEditor>
+      <image>
+        <link>${link}</link>
+        <title>${siteConfig.title}</title>
+        <url>${imageUrl}</url>
+        <width>231</width>
+        <height>50</height>
+      </image>
+    `,
+  })
+}
