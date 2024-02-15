cfc8d91da2333184df102f09797962fa592d33fe
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-02-15 17:13:48 +0100

    Setup indexnow in deploy workflow

---
+++ script/rsync-indexnow.js
@@ -0,0 +1,24 @@
+import { resolve } from 'node:path'
+import { readFile } from 'node:fs/promises'
+
+const fileContent = await readFile(resolve(process.argv[2]), 'utf-8')
+const files = fileContent.split('\n').filter((f) => f.endsWith('.html'))
+if (files.length > 0) {
+  const urlList = files.map((f) => process.env.SITE_URL + '/' + f.replace(/index.html$/, ''))
+  const res = await fetch('https://api.indexnow.org/indexnow', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      host: 'api.indexnow.org',
+    },
+    body: {
+      host: process.env.SITE_URL,
+      key: process.env.INDEXNOW_KEY,
+      keyLocation: process.env.SITE_URL + '/' + process.env.INDEXNOW_KEY + '.txt',
+      urlList,
+    },
+  })
+  if (!res.ok) {
+    console.error(`Sending URLs to indexnow failed with status ${res.status}`)
+  }
+}
