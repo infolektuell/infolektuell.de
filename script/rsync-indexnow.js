9b949effc4cc5f8002bfd6ead40a70769b4513d5
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-02-18 11:43:23 +0100

    Fix fetch call in indexnow script

--- script/rsync-indexnow.js
+++ script/rsync-indexnow.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { resolve } from 'node:path'
 import { readFile } from 'node:fs/promises'
 
@@ -5,20 +6,21 @@ const fileContent = await readFile(resolve(process.argv[2]), 'utf-8')
 const files = fileContent.split('\n').filter((f) => f.endsWith('.html'))
 if (files.length > 0) {
   const urlList = files.map((f) => process.env.SITE_URL + '/' + f.replace(/index.html$/, ''))
+  const body = {
+    host: process.env.SITE_URL,
+    key: process.env.INDEXNOW_KEY,
+    keyLocation: process.env.SITE_URL + '/' + process.env.INDEXNOW_KEY + '.txt',
+    urlList,
+  }
   const res = await fetch('https://api.indexnow.org/indexnow', {
     method: 'post',
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
       host: 'api.indexnow.org',
     },
-    body: {
-      host: process.env.SITE_URL,
-      key: process.env.INDEXNOW_KEY,
-      keyLocation: process.env.SITE_URL + '/' + process.env.INDEXNOW_KEY + '.txt',
-      urlList,
-    },
+    body: JSON.stringify(body),
   })
   if (!res.ok) {
-    console.error(`Sending URLs to indexnow failed with status ${res.status}`)
+    console.error(`Sending URLs to indexnow failed with status ${res.status}: ${res.body}`)
   }
 }
