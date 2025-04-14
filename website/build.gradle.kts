0b5c94784c1bef8b12386deba25e19ccb9b8b31c
Tamara Cook <info@infolektuell.de>
2025-04-14 10:35:06 +0200

    Website: Remove dependency to logos subproject

--- website/build.gradle.kts
+++ website/build.gradle.kts
@@ -1,24 +1,4 @@
-import de.infolektuell.gradle.astro.tasks.AstroBuildTask
-import de.infolektuell.gradle.astro.tasks.AstroCheckTask
-
 plugins {
   id("base")
   id("de.infolektuell.astro")
-}
-
-val websiteLogoTask by project(":logos").tasks.named("compileWebsiteTypstSvg")
-
-val copyLogosTask by tasks.registering(Copy::class) {
-  from(websiteLogoTask)
-  into(layout.projectDirectory.dir("src/assets/logos"))
-  include("*.svg")
-  duplicatesStrategy = DuplicatesStrategy.INCLUDE
-}
-
-tasks.withType(AstroBuildTask::class) {
-  dependsOn(copyLogosTask)
-}
-
-tasks.withType(AstroCheckTask::class) {
-  dependsOn(copyLogosTask)
 }
